f767e4f6ff36d51a63e3880ed637b6fd9d9679eb
Nikita <nikkiravi@yahoo.com>
2025-11-29 23:58:11 -0800

    creating Player.h header file

---
+++ include/models/Player.h
@@ -0,0 +1,21 @@
+//include/models/Player.h
+#pragma once
+#include <string>
+#include <iostream>
+
+class Player{
+    protected:
+        std::string name;
+        std::string nationality;
+        std::string club;
+        std::string position;
+
+        int overallRating;
+        int pace, shooting, passing, dribbling, defending, physicality;
+
+    public:
+        Player(const std::string& name, const std:string& nationality, const std::string& club, const std::string& position, int overall, int pace, int shooting, int passing, int dribbling, int defending, int physicality);
+
+    virtual ~Player() = default;
+
+}
