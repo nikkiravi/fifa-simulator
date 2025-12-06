880ae7751a9e8a96c122a691f22dcb00b9c0e774
nikita.ravi <nikkiravi@yahoo.com>
2025-12-05 22:53:48 -0800

    step 4 and starting of step 5

--- include/models/Player.h
+++ include/models/Player.h
@@ -10,12 +10,12 @@ class Player{
         std::string club;
         std::string position;
 
-        int overallRating;
+        int overall_rating;
         int pace, shooting, passing, dribbling, defending, physicality;
 
     public:
-        Player(const std::string& name, const std:string& nationality, const std::string& club, const std::string& position, int overall, int pace, int shooting, int passing, int dribbling, int defending, int physicality);
+        Player(const std::string& name, const std::string& nationality, const std::string& club, const std::string& position, int overall_rating, int pace, int shooting, int passing, int dribbling, int defending, int physicality);
 
     virtual ~Player() = default;
 
-}
+};
