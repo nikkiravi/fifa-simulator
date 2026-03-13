8ae7dd671a34d12c31ee33595b3e36cb15e0203d
Nikita <nikkiravi@yahoo.com>
2026-03-13 15:50:44 -0700

    fixing milestone1 bugs

--- src/main.cpp
+++ src/main.cpp
@@ -3,7 +3,7 @@
 #include "models/AttackerPlayer.h"
 
 int main() {
-  auto messi = std::make_unique<AttackPlayer>("Lionel Messi", "Argentina", "Barcelona", "LW", 91, 97, 88, 82, 92, 39, 78);
+  auto messi = std::make_unique<AttackerPlayer>("Lionel Messi", "Argentina", "Barcelona", "LW", 91, 97, 88, 82, 92, 39, 78);
   messi->displayInfo();
   std::cout << "\nMilestone 1 is Complete!\n";
   return 0;
